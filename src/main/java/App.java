6d09fa816405dc93c37fbf4bba131335ce2f67c2
Alexey Levashev <alex.levashev@gmail.com>
2022-01-11 16:38:59 +0100

    Args added, legend for service creation added, useless output removed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.Map;
+
 //cat ./usr/lib/systemd/system/slackbot_mt_monitor.service
 //        [Unit]
 //        Description=Maintenance Queueu Monitor
@@ -18,12 +20,35 @@ import java.util.HashMap;
 //cat service_start.sh
 //        #/bin/bash
 //        /usr/bin/java -jar /var/www/html/slackbot-java/SlackNotificationBot.jar
-
 public class App {
-    private final static String telegramToken = "";
-    private final static String chatId = "";
+    private static Map<String, String> map;
+
+    private static void makeMap(String[] args) {
+        map = new HashMap<>();
+        for (String arg : args) {
+            if (arg.contains("-")) {
+                if (arg.contains("=")) {
+                    String tmp_key = arg.substring(1, arg.indexOf('='));
+                    String tmp_val = arg.substring(arg.indexOf('=') + 1);
+                    map.put(tmp_key, tmp_val);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
-        HashMap<String, String> serverList = Servers.readCSV("servers.csv");
+        makeMap(args);
+
+        if (!map.containsKey("apiToken") || !map.containsKey("chatId") || !map.containsKey("file")) {
+            System.out.println("Params error!");
+            System.exit(0);
+        }
+
+        String telegramToken = map.get("apiToken");
+        String chatId = map.get("chatId");
+        String file = map.get("file");
+
+        HashMap<String, String> serverList = Servers.readCSV(file);
         HashMap<String, Boolean> serverStatus = new HashMap<>();
         Telegram telegramClient = new Telegram(telegramToken, chatId);
         Boolean firstRunFlag = true;
@@ -31,23 +56,20 @@ public class App {
             for (HashMap.Entry<String, String> entry : serverList.entrySet()) {
                 Boolean currentServerStatus =  Servers.isAlive(entry.getValue());
                 if(!serverStatus.containsKey(entry.getKey())) {
-                    System.out.println("Adding key " + entry.getKey() + " to the list of checked servers " +
-                            "with status " + currentServerStatus.toString());
                     serverStatus.put(entry.getKey(), currentServerStatus);
                 } else {
-                    if(serverStatus.get(entry.getKey()) == currentServerStatus) {
-                        System.out.println("Status of " + entry.getKey() + " is the same");
-                    } else {
-                        System.out.println("Status of " + entry.getKey() + " is changed from " + serverStatus.get(entry.getKey()) + " to " + currentServerStatus);
+                    if(serverStatus.get(entry.getKey()) != currentServerStatus) {
                         serverStatus.replace(entry.getKey(), currentServerStatus);
-                        telegramClient.send("Status of <br>" + entry.getKey() + "</b> is changed from " + serverStatus.get(entry.getKey()) + " to " + currentServerStatus);
+                        telegramClient.send("Status of <b>" + entry.getKey() + "</b> is changed from <b>"
+                                + serverStatus.get(entry.getKey()) + "</b> to <b>" + currentServerStatus + "</b>");
                     }
                 }
             }
             if(firstRunFlag) {
                 String message = new String();
                 for (HashMap.Entry<String, Boolean> item : serverStatus.entrySet()) {
-                    message += item.getKey() + ". Alive - " + item.getValue().toString().toUpperCase() + "\n";
+                    message += "<b>" + item.getKey() + "</b>. Alive - <b>"
+                            + item.getValue().toString().toUpperCase() + "</b>\n";
                 }
                 telegramClient.send("<b>Server status</b>\n" + message);
                 firstRunFlag = false;
